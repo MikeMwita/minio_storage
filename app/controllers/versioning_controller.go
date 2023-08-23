8c55f0011cfbcd23a5b1b38f265ac12edc1c26c0
MikeMwita <mwitamike8@gmail.com>
2023-08-23 22:24:27 +0300

    versioning

--- app/controllers/versioning_controller.go
+++ app/controllers/versioning_controller.go
@@ -1,1 +1,60 @@
 package controllers
+
+import (
+	"context"
+	"fmt"
+	minioUpload "github.com/Filtronic/Minio/platform/minio"
+	"github.com/gofiber/fiber/v2"
+)
+
+func EnableVersioning(c *fiber.Ctx) error {
+	ctx := context.Background()
+	bucketName := c.Params("bucketName")
+	minioClient, err := minioUpload.MinioConnection()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	err = minioClient.EnableVersioning(ctx, bucketName)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"error":   false,
+		"message": fmt.Sprintf("Versioning enabled for bucket %s", bucketName),
+	})
+}
+
+//disable bucket versioning
+
+//func DisableVersioning(c *fiber.Ctx) error {
+//	ctx := context.Background()
+//	bucketName := c.Params("bucketName")
+//	minioClient, err := minioUpload.MinioConnection()
+//	if err != nil {
+//		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+//			"error": true,
+//			"msg":   err.Error(),
+//		})
+//	}
+//
+//	err = minioClient.DisableVersioning(ctx, bucketName)
+//	if err != nil {
+//		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+//			"error": true,
+//			"msg":   err.Error(),
+//		})
+//	}
+//
+//	return c.JSON(fiber.Map{
+//		"error":   false,
+//		"message": fmt.Sprintf("Versioning disabled for bucket %s", bucketName),
+//	})
+//}
