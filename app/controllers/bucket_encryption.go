ebd892b354897c5714352b909430f4617217a94a
MikeMwita <mwitamike8@gmail.com>
2023-08-24 13:33:57 +0300

    bucket encryption controller

--- app/controllers/bucket_encryption.go
+++ app/controllers/bucket_encryption.go
@@ -1,1 +1,61 @@
 package controllers
+
+import (
+	"context"
+	"fmt"
+	minioUpload "github.com/Filtronic/Minio/platform/minio"
+	"github.com/gofiber/fiber/v2"
+)
+
+// get bucket encryption
+
+func GetBucketEncryption(c *fiber.Ctx) error {
+	ctx := context.Background()
+	bucketName := c.Params("bucketName")
+	minioClient, err := minioUpload.MinioConnection()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+	// Get default encryption configuration
+	encryptionConfig, err := minioClient.GetBucketEncryption(ctx, bucketName)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+	return c.JSON(fiber.Map{
+		"error":  false,
+		"config": encryptionConfig,
+		"msg":    "Default encryption configuration retrieved successfully.",
+	})
+}
+
+// remove bucket encryption
+
+func RemoveBucketEncryption(c *fiber.Ctx) error {
+	ctx := context.Background()
+	bucketName := c.Params("bucketName")
+	minioClient, err := minioUpload.MinioConnection()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+	err = minioClient.RemoveBucketEncryption(ctx, bucketName)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"error": false,
+		"msg":   fmt.Sprintf("Default encryption configuration removed from bucket '%s'.", bucketName),
+	})
+}
