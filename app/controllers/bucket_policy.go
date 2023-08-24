5ed226f7ca9903d6b7e6fe09ec204556e016dc21
MikeMwita <mwitamike8@gmail.com>
2023-08-24 13:33:20 +0300

    bucket policy controler

--- app/controllers/bucket_policy.go
+++ app/controllers/bucket_policy.go
@@ -1,1 +1,69 @@
 package controllers
+
+import (
+	"context"
+	"fmt"
+	minioUpload "github.com/Filtronic/Minio/platform/minio"
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetBucketPolicy(c *fiber.Ctx) error {
+	ctx := context.Background()
+	bucketName := c.Params("bucketName")
+
+	// Get the policy from the request body
+	var policy string
+	if err := c.BodyParser(&policy); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   "Invalid request body format",
+		})
+	}
+	minioClient, err := minioUpload.MinioConnection()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	// Set access policy on the bucket
+	err = minioClient.SetBucketPolicy(ctx, bucketName, policy)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"error": false,
+		"msg":   fmt.Sprintf("Access policy set on bucket '%s'.", bucketName),
+	})
+}
+
+// get bucket policy
+
+func GetBucketPolicy(c *fiber.Ctx) error {
+	ctx := context.Background()
+	bucketName := c.Params("bucketName")
+	minioClient, err := minioUpload.MinioConnection()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+	// Get the access policy on the bucket
+	policy, err := minioClient.GetBucketPolicy(ctx, bucketName)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+	return c.JSON(fiber.Map{
+		"error":  false,
+		"policy": policy,
+	})
+}
