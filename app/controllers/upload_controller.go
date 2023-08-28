144bda5883e76d10c908569e667946cdc8045396
MikeMwita <mwitamike8@gmail.com>
2023-08-28 15:05:34 +0300

    modified upload

--- app/controllers/upload_controller.go
+++ app/controllers/upload_controller.go
@@ -2,13 +2,60 @@ package controllers
 
 import (
 	"context"
+	"github.com/Filtronic/Minio/app/models"
 	minioUpload "github.com/Filtronic/Minio/platform/minio"
 	"github.com/gofiber/fiber/v2"
 	"github.com/minio/minio-go/v7"
+	"gorm.io/gorm"
 	"log"
+	"time"
 )
 
-func UploadFile(c *fiber.Ctx) error {
+//func UploadFile(c *fiber.Ctx) error {
+//	ctx := context.Background()
+//	bucketName := "filtronic"
+//	file, err := c.FormFile("fileUpload")
+//	if err != nil {
+//		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+//			"error": true,
+//			"msg":   err.Error(),
+//		})
+//	}
+//	buffer, err := file.Open()
+//	if err != nil {
+//		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+//			"error": true,
+//			"msg":   err.Error(),
+//		})
+//	}
+//	defer buffer.Close()
+//	minioClient, err := minioUpload.MinioConnection()
+//	if err != nil {
+//		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+//			"error": true,
+//			"msg":   err.Error(),
+//		})
+//	}
+//	objectName := file.Filename
+//	fileBuffer := buffer
+//	contentType := file.Header["Content-Type"][0]
+//	fileSize := file.Size
+//	info, err := minioClient.PutObject(ctx, bucketName, objectName, fileBuffer, fileSize, minio.PutObjectOptions{ContentType: contentType})
+//	if err != nil {
+//		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+//			"error": true,
+//			"msg":   err.Error(),
+//		})
+//	}
+//	log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+//	return c.JSON(fiber.Map{
+//		"error": false,
+//		"msg":   nil,
+//		"info":  info,
+//	})
+//}
+
+func UploadFile(c *fiber.Ctx, db *gorm.DB) error {
 	ctx := context.Background()
 	bucketName := "filtronic"
 	file, err := c.FormFile("fileUpload")
@@ -45,9 +92,23 @@ func UploadFile(c *fiber.Ctx) error {
 		})
 	}
 	log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+
+	// Store metadata in the database
+	fileMetadata := models.FileMetadata{
+		FileName:   objectName,
+		UploaderID: 123, // Example user ID
+		UploadTime: time.Now(),
+		// ... Other metadata fields
+	}
+	if err := models.CreateFileMetadata(db, fileMetadata); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
 	return c.JSON(fiber.Map{
 		"error": false,
-		"msg":   nil,
+		"msg":   "File uploaded successfully.",
 		"info":  info,
 	})
 }
