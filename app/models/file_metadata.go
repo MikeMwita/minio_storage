2262bdbb9877295ac92d1ed30b4e05b7ac70397d
MikeMwita <mwitamike8@gmail.com>
2023-08-28 15:06:06 +0300

    metadata file

--- app/models/file_metadata.go
+++ app/models/file_metadata.go
@@ -1,1 +1,26 @@
 package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type FileMetadata struct {
+	gorm.Model
+	FileName   string
+	UploaderID int
+	UploadTime time.Time
+}
+
+func CreateFileMetadata(db *gorm.DB, metadata FileMetadata) error {
+	return db.Create(&metadata).Error
+}
+
+func GetFileMetadataList(db *gorm.DB) ([]FileMetadata, error) {
+	var metadataList []FileMetadata
+	result := db.Find(&metadataList)
+	if result.Error != nil && result.Error != gorm.ErrRecordNotFound {
+		return nil, result.Error
+	}
+	return metadataList, nil
+}
