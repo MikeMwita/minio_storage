829ba3e91f68df0da8bb1e65c7252cb8cdeabcb4
MikeMwita <mwitamike8@gmail.com>
2023-08-28 15:04:36 +0300

    metadata storage

--- app/controllers/metadata.go
+++ app/controllers/metadata.go
@@ -1,1 +1,46 @@
 package controllers
+
+import (
+	"github.com/Filtronic/Minio/app/models"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+//	func GetFileMetadata(c *fiber.Ctx) error {
+//		db := c.Locals("db").(*gorm.DB)
+//		// Fetch metadata from the database
+//		metadataList, err := models.GetFileMetadataList(db)
+//		if err != nil {
+//			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+//				"error": true,
+//				"msg":   err.Error(),
+//			})
+//		}
+//		return c.JSON(fiber.Map{
+//			"error":    false,
+//			"metadata": metadataList,
+//		})
+//	}
+
+func GetFileMetadata(c *fiber.Ctx) error {
+	// Get the 'db' instance from the fiber context
+	db := c.Locals("db").(*gorm.DB)
+	if db == nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   "Database connection is not found",
+		})
+	}
+	// Fetch metadata from the database
+	metadataList, err := models.GetFileMetadataList(db)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+	return c.JSON(fiber.Map{
+		"error":    false,
+		"metadata": metadataList,
+	})
+}
