398fb85c0e1d21c57ea8b8512d23711270a23354
MikeMwita <mwitamike8@gmail.com>
2023-08-29 17:03:23 +0300

    updated upload

--- app/controllers/upload_controller.go
+++ app/controllers/upload_controller.go
@@ -11,50 +11,6 @@ import (
 	"time"
 )
 
-//func UploadFile(c *fiber.Ctx) error {
-//	ctx := context.Background()
-//	bucketName := "filtronic"
-//	file, err := c.FormFile("fileUpload")
-//	if err != nil {
-//		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-//			"error": true,
-//			"msg":   err.Error(),
-//		})
-//	}
-//	buffer, err := file.Open()
-//	if err != nil {
-//		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-//			"error": true,
-//			"msg":   err.Error(),
-//		})
-//	}
-//	defer buffer.Close()
-//	minioClient, err := minioUpload.MinioConnection()
-//	if err != nil {
-//		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-//			"error": true,
-//			"msg":   err.Error(),
-//		})
-//	}
-//	objectName := file.Filename
-//	fileBuffer := buffer
-//	contentType := file.Header["Content-Type"][0]
-//	fileSize := file.Size
-//	info, err := minioClient.PutObject(ctx, bucketName, objectName, fileBuffer, fileSize, minio.PutObjectOptions{ContentType: contentType})
-//	if err != nil {
-//		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-//			"error": true,
-//			"msg":   err.Error(),
-//		})
-//	}
-//	log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
-//	return c.JSON(fiber.Map{
-//		"error": false,
-//		"msg":   nil,
-//		"info":  info,
-//	})
-//}
-
 func UploadFile(c *fiber.Ctx, db *gorm.DB) error {
 	ctx := context.Background()
 	bucketName := "filtronic"
@@ -96,9 +52,10 @@ func UploadFile(c *fiber.Ctx, db *gorm.DB) error {
 	// Store metadata in the database
 	fileMetadata := models.FileMetadata{
 		FileName:   objectName,
-		UploaderID: 123, // Example user ID
+		UploaderID: 123,
 		UploadTime: time.Now(),
-		// ... Other metadata fields
+		ETag:       info.ETag,
+		VersionID:  info.VersionID,
 	}
 	if err := models.CreateFileMetadata(db, fileMetadata); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
