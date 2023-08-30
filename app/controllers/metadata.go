966de53d297c6e65d56b3785ae6b44f3d422b6ac
MikeMwita <mwitamike8@gmail.com>
2023-08-30 16:33:06 +0300

    implemented metadata

--- app/controllers/metadata.go
+++ app/controllers/metadata.go
@@ -26,3 +26,24 @@ func GetFileMetadata(c *fiber.Ctx) error {
 		"metadata": metadataList,
 	})
 }
+
+func GetBucketMetadata(c *fiber.Ctx) error {
+	db := c.Locals("db").(*gorm.DB)
+	if db == nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   "Database connection is not found",
+		})
+	}
+	metadataList, err := models.GetBucketMetadataList(db)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+	return c.JSON(fiber.Map{
+		"error":    false,
+		"metadata": metadataList,
+	})
+}
