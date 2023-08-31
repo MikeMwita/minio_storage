10bcfb23711dab64c4ede152b892bc19020d8afc
MikeMwita <mwitamike8@gmail.com>
2023-08-31 23:56:14 +0300

    Update config

--- util/config.go
+++ util/config.go
@@ -1,1 +1,24 @@
 package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	HTTPServerAddress string `mapstructure:"HTTP_SERVER_ADDRESS"`
+	GRPCServerAddress string `mapstructure:"GRPC_SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
