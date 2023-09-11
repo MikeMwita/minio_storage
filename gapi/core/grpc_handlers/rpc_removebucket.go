8e57fb4ae4f2d247cdde13ece08f7b66c75e0b8a
MikeMwita <mwitamike8@gmail.com>
2023-09-11 21:38:37 +0300

    added removebucket rpc

--- gapi/core/grpc_handlers/rpc_removebucket.go
+++ gapi/core/grpc_handlers/rpc_removebucket.go
@@ -1,1 +1,33 @@
 package grpc_handlers
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/Filtronic/Minio/gapi/pb/mutation_gen"
+	minioUpload "github.com/Filtronic/Minio/platform/minio"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *grpcServer) RemoveBucket(ctx context.Context, req *pb.RemoveBucketRequest) (*pb.RemoveBucketResponse, error) {
+	bucketName := req.GetBucketName()
+
+	minioClient, err := minioUpload.MinioConnection()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error connecting to Minio: %v", err)
+	}
+	if err := minioClient.RemoveBucket(ctx, bucketName); err != nil {
+		return nil, status.Errorf(codes.Internal, "error removing bucket: %v", err)
+	}
+
+	bucketTag := "buck01"
+	uniqueID := "xyz123"
+
+	return &pb.RemoveBucketResponse{
+		Success:    true,
+		BucketName: bucketName,
+		BucketTag:  bucketTag,
+		UniqueId:   uniqueID,
+		Message:    fmt.Sprintf("Bucket %s has been successfully removed", bucketName),
+	}, nil
+}
