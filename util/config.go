d1c63ed5f176f1a4db9cbd69cc44f534212ed5c3
MikeMwita <mwitamike8@gmail.com>
2023-09-17 15:31:20 +0300

    modified config

--- util/config.go
+++ util/config.go
@@ -11,25 +11,15 @@ type Config struct {
 }
 
 func LoadConfig(path string) (config Config, err error) {
-	//viper.AddConfigPath(path)
-	//viper.SetConfigName("app")
 	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Error reading .env file: %v", err)
 	}
-
-	// Unmarshal the configuration into the Config struct
 	if err := viper.Unmarshal(&config); err != nil {
 		log.Fatalf("Error unmarshalling configuration: %v", err)
 	}
 
 	return config, nil
-	//err = viper.ReadInConfig()
-	//if err != nil {
-	//	return
-	//}
-	//
-	//err = viper.Unmarshal(&config)
-	//return
+
 }
