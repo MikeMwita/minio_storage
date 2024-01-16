69576594ab36f3c517307b18255c9a22fd6c803a
MikeMwita <mwitamike8@gmail.com>
2024-01-16 15:40:13 +0300

    add test file for list_buckets  controller

--- app/controllers/list_buckets_controller_test.go
+++ app/controllers/list_buckets_controller_test.go
@@ -1,23 +1,54 @@
 package controllers
 
-import "testing"
+import (
+	"context"
+	"github.com/gofiber/fiber/v2"
+	"github.com/minio/minio-go/v7"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+type MockMinioClient struct{}
+
+func (m *MockMinioClient) ListBuckets(ctx context.Context) ([]minio.BucketInfo, error) {
+	// Mock data for testing
+	bucketList := []minio.BucketInfo{
+		{Name: "bucket1", CreationDate: time.Now()},
+		{Name: "bucket2", CreationDate: time.Now().Add(-24 * time.Hour)},
+	}
+
+	return bucketList, nil
+}
 
 func TestListBuckets(t *testing.T) {
 	type args struct {
 		c *fiber.Ctx
 	}
+
 	tests := []struct {
 		name    string
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "List buckets successfully",
+			args: args{
+				c: &fiber.Ctx{},
+			},
+			wantErr: false,
+		},
 	}
+
+	//// Override the MinioConnection function with the mock Minio client
+	//minioUpload.MinioConnection = func() (minioUpload.MinioClient, error) {
+	//	return &MockMinioClient{}, nil
+	//}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := ListBuckets(tt.args.c); (err != nil) != tt.wantErr {
-				t.Errorf("ListBuckets() error = %v, wantErr %v", err, tt.wantErr)
-			}
+			err := ListBuckets(tt.args.c)
+			assert.Equal(t, (err != nil), tt.wantErr, "ListBuckets() error")
 		})
 	}
 }
