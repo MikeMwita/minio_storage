a63ed23b9c68f0c5af013e902f294f928ce8d060
MikeMwita <mwitamike8@gmail.com>
2024-01-16 15:39:10 +0300

    add test file for bucket policy  controller

--- app/controllers/bucket_policy_test.go
+++ app/controllers/bucket_policy_test.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "testing"
+import (
+	"context"
+	"github.com/gofiber/fiber/v2"
+	"github.com/valyala/fasthttp"
+	"testing"
+)
 
 func TestGetBucketPolicy(t *testing.T) {
 	type args struct {
@@ -11,7 +16,27 @@ func TestGetBucketPolicy(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Valid bucket name",
+			args: args{
+				c: mockFiberCtx("my-bucket"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Invalid bucket name",
+			args: args{
+				c: mockFiberCtx("non-existent-bucket"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Empty bucket name",
+			args: args{
+				c: mockFiberCtx(""),
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -31,7 +56,34 @@ func TestSetBucketPolicy(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Valid bucket name",
+			args: args{
+				c: mockFiberCtxWithBody("my-bucket", `{"policy":"sample-policy"}`),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Invalid bucket name",
+			args: args{
+				c: mockFiberCtxWithBody("non-existent-bucket", `{"policy":"sample-policy"}`),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Empty bucket name",
+			args: args{
+				c: mockFiberCtxWithBody("", `{"policy":"sample-policy"}`),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid request body",
+			args: args{
+				c: mockFiberCtxWithBody("my-bucket", `invalid-json`),
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -40,4 +92,21 @@ func TestSetBucketPolicy(t *testing.T) {
 			}
 		})
 	}
+}
+
+func mockFiberCtxWithBody(bucketName, requestBody string) *fiber.Ctx {
+	app := fiber.New()
+
+	ctx := &fasthttp.RequestCtx{}
+	ctx.Request.Header.SetMethod("POST")
+	ctx.Request.SetRequestURI("/" + bucketName)
+	ctx.Request.SetBodyString(requestBody)
+
+	c := app.AcquireCtx(ctx)
+
+	c.Locals("user", context.Background())
+
+	c.Params("bucket_name", bucketName)
+
+	return c
 }
